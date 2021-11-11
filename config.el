b0b2078d6092c87ca9e0ca38a0142fd2c220b846
disconsis <ketan.kanishka@nyu.edu>
2021-11-12 01:43:44 +0530

    add personal info

--- config.el
+++ config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name    "Ketan Kanishka"
+      user-mail-address "ketan.kanishka@nyu.edu")
 
 (setq doom-font (font-spec :family "Iosevka" :size 12))
 

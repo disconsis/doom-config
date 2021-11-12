10130e201a89fa3494b0f2af6eef2ec1b29915de
disconsis <ketan.kanishka@nyu.edu>
2021-11-12 13:41:37 +0530

    demarcate outline heading

--- config.el
+++ config.el
@@ -1,20 +1,9 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name    "Ketan Kanishka"
       user-mail-address "ketan.kanishka@nyu.edu")
-
-(setq doom-font (font-spec :family "Iosevka" :size 12))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -34,3 +23,8 @@
 (setq org-hide-emphasis-markers t)
 
 (map! :leader "SPC" #'counsel-M-x)
+
+;;; $ UI
+
+;;; $$ Font
+(setq doom-font (font-spec :family "Iosevka" :size 12))

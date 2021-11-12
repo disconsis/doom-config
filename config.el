b425cfdf281ab9aa3363b9f2f466de15237bc4f8
disconsis <ketan.kanishka@nyu.edu>
2021-11-12 13:41:52 +0530

    add theme progression

--- config.el
+++ config.el
@@ -28,3 +28,43 @@
 
 ;;; $$ Font
 (setq doom-font (font-spec :family "Iosevka" :size 12))
+
+;;; $$ Theme
+
+(defun pm (hour) (mod (+ hour 12) 24))
+(defun am (hour) hour)
+(defun between (start item end)
+  (and (<= start item)
+       (<= item end)))
+
+(setq kk/theme-timings `((doom-flatwhite         . (,(am 6) . ,(pm 3)))
+                         (doom-monokai-ristretto . (,(pm 4) . ,(pm 8)))
+                         (doom-homage-black      . (,(pm 9) . ,(pm 12)))
+                         (doom-ayu-mirage        . (,(am 1) . ,(am 5)))))
+
+(defun kk/theme-for-time ()
+  "Get appropriate theme for current time from `kk/theme-timings'."
+  (let* ((curr-hour (decoded-time-hour (decode-time (current-time))))
+         (check-theme
+            (lambda (item)
+              (cl-destructuring-bind (_theme . (start-hour . end-hour)) item
+                  (if (<= start-hour end-hour)
+                      (between start-hour curr-hour end-hour)
+                    (or (between end-hour curr-hour 23)
+                        (between 0 curr-hour end-hour))))))
+         (found-item (seq-find check-theme kk/theme-timings))
+         (default-theme 'default))
+    (if (not found-item)
+        (progn
+          (warn "No suitable theme found in `kk/theme-timings' for hour = %s. Defaulting to %s theme" curr-hour default-theme)
+          default-theme)
+      (car found-item))))
+
+(setq doom-theme (kk/theme-for-time))
+
+(defun kk/reload-theme-for-time ()
+  (setq doom-theme (kk/theme-for-time))
+  (doom/reload-theme))
+
+;; check every hour
+(run-at-time t 3600 #'kk/reload-theme-for-time)

c143e5eac51652514b07ed521445331ee77c9878
disconsis <ketan.kanishka@nyu.edu>
2022-09-18 22:25:27 +0530

    enable features

--- init.el
+++ init.el
@@ -39,13 +39,13 @@
        ;;minimap            ; show a map of the code on the side
        modeline             ; snazzy, Atom-inspired modeline, plus API
        nav-flash            ; blink cursor line after big motions
-       ;;neotree            ; a project drawer, like NERDTree for vim
+       neotree              ; a project drawer, like NERDTree for vim
        ophints              ; highlight the region an operation acts on
        (popup +defaults)    ; tame sudden yet inevitable temporary windows
        ;;tabs               ; a tab bar for Emacs
        ;;treemacs           ; a project drawer, like neotree but cooler
        ;;unicode            ; extended unicode support for various languages
-       vc-gutter            ; vcs diff in the fringe
+       (vc-gutter +pretty)  ; vcs diff in the fringe
        ;;vi-tilde-fringe    ; fringe tildes to mark beyond EOB
        ;;window-select      ; visually switch windows
        workspaces           ; tab emulation, persistence & separate workspaces
@@ -57,10 +57,10 @@
        fold                 ; (nigh) universal code folding
        format               ; automated prettiness
        ;;god                ; run Emacs commands without modifier keys
-       lispy                ; vim for lisp, for people who don't like vim
+       ;;lispy              ; vim for lisp, for people who don't like vim
        ;;multiple-cursors   ; editing in many places at once
        ;;objed              ; text object editing for the innocent
-       ;;parinfer           ; turn lisp into python, sort of
+       parinfer             ; turn lisp into python, sort of
        ;;rotate-text        ; cycle region at point between text candidates
        snippets             ; my elves. They type so I don't have to
        ;;word-wrap          ; soft wrapping with language-aware indent
@@ -113,7 +113,7 @@
        ;;agda               ; types of types of types of types...
        ;;beancount          ; mind the GAAP
        ;;cc                 ; C > C++ == 1
-       ;;clojure            ; java with a lisp
+       clojure              ; java with a lisp
        ;;common-lisp        ; if you've seen one lisp, you've seen them all
        ;;coq                ; proofs-as-programs
        ;;crystal            ; ruby at the speed of c
@@ -131,7 +131,7 @@
        ;;fstar              ; (dependent) types and (monadic) effects and Z3
        ;;gdscript           ; the language you waited for
        ;;(go +lsp)          ; the hipster dialect
-       ;;(haskell +dante)   ; a language that's lazier than I am
+       (haskell +lsp)       ; a language that's lazier than I am
        ;;hy                 ; readability of scheme w/ speed of python
        ;;idris              ; a language you can depend on
        json                 ; At least it ain't XML
